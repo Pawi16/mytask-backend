d34e61056be67cf933499f367937c3e032364a8a
Pawin <cap.pawin@gmail.com>
2025-04-03 19:01:29 +0700

    feat: create mapper for login response

--- src/main/java/com/pawi16/taskapp/taskapp/mapper/UserMapper.java
+++ src/main/java/com/pawi16/taskapp/taskapp/mapper/UserMapper.java
@@ -1,11 +1,16 @@
 package com.pawi16.taskapp.taskapp.mapper;
 
 import com.pawi16.taskapp.taskapp.entity.User;
+import com.pawi16.taskapp.taskapp.model.LoginResponse;
 import com.pawi16.taskapp.taskapp.model.RegisterResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper{
 
     RegisterResponse userToRegisterResponse (User user);
+
+    @Mapping(target = "token", ignore = true)
+    LoginResponse userToLoginResponse (User user);
 }
