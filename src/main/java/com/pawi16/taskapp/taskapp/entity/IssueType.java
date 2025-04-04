c4d53a9fb7d106406c20d0c2d051e840a15acb73
Pawin <cap.pawin@gmail.com>
2025-04-05 03:10:04 +0700

    feat: create type for priority and issue

---
+++ src/main/java/com/pawi16/taskapp/taskapp/entity/IssueType.java
@@ -0,0 +1,5 @@
+package com.pawi16.taskapp.taskapp.entity;
+
+public enum IssueType {
+    EVENT, TASK, SUB_TASK
+}
---
+++ src/main/java/com/pawi16/taskapp/taskapp/entity/PriorityType.java
@@ -0,0 +1,5 @@
+package com.pawi16.taskapp.taskapp.entity;
+
+public enum PriorityType {
+    LOW, MEDIUM, HIGH
+}
