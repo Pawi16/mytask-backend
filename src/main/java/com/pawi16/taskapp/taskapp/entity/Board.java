b2d7609ccc215c2c313c2cb337ec5d6da8e86780
Pawin <cap.pawin@gmail.com>
2025-04-05 03:09:34 +0700

    feat: create board entity

---
+++ src/main/java/com/pawi16/taskapp/taskapp/entity/Board.java
@@ -0,0 +1,22 @@
+package com.pawi16.taskapp.taskapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity(name = "m_board")
+public class Board extends BaseEntity{
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "m_created_by", nullable = false)
+    private User createdUser;
+
+    @OneToMany(mappedBy = "board",cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Issue> issues;
+}
