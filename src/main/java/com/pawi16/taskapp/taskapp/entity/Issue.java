e612a5c023bfcef9fd2b5871b2804c27d56eaab8
Pawin <cap.pawin@gmail.com>
2025-04-05 03:08:51 +0700

    fix: rename task to issue and modify field to be compatible with board

---
+++ src/main/java/com/pawi16/taskapp/taskapp/entity/Issue.java
@@ -0,0 +1,54 @@
+package com.pawi16.taskapp.taskapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true, exclude = {"parentIssue", "childIssues"})
+@ToString(exclude = {"parentIssue", "childIssues"})
+@Data
+@Entity(name = "m_task")
+public class Issue extends BaseEntity {
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Column
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    private IssueType issueType;
+
+    @Column(nullable = false)
+    private boolean isCompleted;
+
+    @Enumerated(EnumType.STRING)
+    private PriorityType priority;
+
+    @ManyToOne
+    @JoinColumn(name = "m_created_by", nullable = false)
+    private User createdUser;
+
+    @ManyToOne
+    @JoinColumn(name = "m_parent_issue")
+    private Issue parentIssue;
+
+    @ManyToOne
+    @JoinColumn(name = "m_board_id")
+    private Board board;
+
+    @OneToMany(mappedBy = "parentIssue",cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Issue> childIssues;
+
+}
+
