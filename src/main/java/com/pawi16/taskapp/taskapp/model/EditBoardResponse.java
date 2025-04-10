8752953ecd65181508d226619f2a9d308f591ca8
Pawin <cap.pawin@gmail.com>
2025-04-11 06:23:53 +0700

    fix: include updateAt in EditBoardResponse

--- src/main/java/com/pawi16/taskapp/taskapp/model/EditBoardResponse.java
+++ src/main/java/com/pawi16/taskapp/taskapp/model/EditBoardResponse.java
@@ -2,8 +2,11 @@ package com.pawi16.taskapp.taskapp.model;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class EditBoardResponse {
     private String title;
     private String message;
+    private LocalDate updateAt;
 }
